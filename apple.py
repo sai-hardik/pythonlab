10763da076fc503dcbcd9e07ef002b18ce0d8f40
Sai Hardik <74413544+sai-hardik@users.noreply.github.com>
2021-01-06 13:14:25 +0530

    Add files via upload

---
+++ apple.py
@@ -0,0 +1,5 @@
+n=int(input('enter the no. of students :'))
+k=int(input('enter the no. of apples :'))
+v=k//n #no. of apples each students will get
+r=k%n  #remaining no. of apples
+print('the no. of apples which each student will get :',v,'remaining no. of apples',r)
