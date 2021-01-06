df326926d280eeb2ac63abe5782055b144f22493
sai-hardik <74413544+sai-hardik@users.noreply.github.com>
2021-01-06 12:40:08 +0530

    Add files via upload

---
+++ SI.py
@@ -0,0 +1,6 @@
+p=float(input("Enter the principal amount: "))
+t=int(input("Enter the time period(in years): "))
+r=float(input("Enter the rate of interest: "))
+SI=(p*t*r)/100 #formula for calculating the simple interest
+print("\n")
+print("The simple interest for the account is",SI)
